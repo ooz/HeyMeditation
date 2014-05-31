79f9757e52896232ec3dc1c43f828b49b9f93e9f
ooz <oliverzscheyge@gmail.com>
2014-05-31 15:04:00 +0200

    Initial commit.

---
+++ src/com/github/ooz/heymeditation/MainActivity.java
@@ -0,0 +1,54 @@
+package com.github.ooz.heymeditation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	public final static String TIMER_MESSAGE = "com.github.ooz.heymeditation.TIMER_MESSAGE";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		// action with ID action_refresh was selected
+		case R.id.action_about:
+			Toast.makeText(this, "Hey! Meditation by Oliver Zeit\nIcon created with the Android Asset Studio", Toast.LENGTH_LONG).show();
+			break;
+		default:
+			break;
+		}
+
+		return true;
+	}
+	
+	public void sendTime(View view) {
+		Intent intent = new Intent(this, TimerActivity.class);
+		
+		if (R.id.button1 == view.getId()) {
+			intent.putExtra(TIMER_MESSAGE, 1);
+		} else if (R.id.button2 == view.getId()) {
+			intent.putExtra(TIMER_MESSAGE, 3);
+		}
+		
+		startActivity(intent);
+	}
+}
---
+++ src/com/github/ooz/heymeditation/TimerActivity.java
@@ -0,0 +1,136 @@
+package com.github.ooz.heymeditation;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TimerActivity extends Activity {
+	
+	private final static int NUM_MESSAGES = 3;
+	private final static int DEFAULT_DURATION = 1;
+	
+	private CountDownTimer preparationTimer = null;
+	private CountDownTimer meditationTimer = null;
+	private int meditationDuration = TimerActivity.DEFAULT_DURATION;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timer);
+		
+		Intent intent = getIntent();
+		this.meditationDuration = intent.getIntExtra(MainActivity.TIMER_MESSAGE, 
+													 TimerActivity.DEFAULT_DURATION);
+		
+		createTimers();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		// action with ID action_refresh was selected
+		case R.id.action_about:
+			Toast.makeText(this, "Hey! Meditation by Oliver Zeit\nIcon created with the Android Asset Studio", Toast.LENGTH_LONG).show();
+			break;
+		default:
+			break;
+		}
+
+		return true;
+	}
+	
+	private void createTimers() {
+		final TextView text = (TextView) findViewById(R.id.textView1);
+		
+		this.preparationTimer = new CountDownTimer(8000, 1000) {
+			@Override
+			public void onTick(long millisUntilFinished) {
+				if (millisUntilFinished <= 4000) {
+					text.setText("Close your eyes!");
+				}
+			}
+			
+			@Override
+			public void onFinish() {
+				meditationTimer = new CountDownTimer(meditationDuration * 60 * 1000, 1000) {
+					@Override
+					public void onTick(long millisUntilFinished) {
+						text.setText("" + millisUntilFinished / 1000);
+					}
+					
+					@Override
+					public void onFinish() {
+						Random rand = new Random();
+						int msgNumber = rand.nextInt(TimerActivity.NUM_MESSAGES);
+						switch (msgNumber) {
+						case 0:
+							text.setText("Hey! You are perfect!");
+							break;
+						case 1:
+							text.setText("Hey! You can do it!");
+							break;
+						default:
+							text.setText("Hey!");
+						}
+						
+						// Play the notification sound. Taken from:
+						// http://stackoverflow.com/questions/4441334/how-to-play-an-android-notification-sound
+						try {
+						    Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+						    Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+						    r.play();
+						} catch (Exception e) {
+						    e.printStackTrace();
+						}
+					}
+				};
+				meditationTimer.start();
+			}
+		};
+		this.preparationTimer.start();
+	}
+	
+//	@Override
+//	protected void onStart() {
+//		super.onStart();
+//		
+//	}
+	
+//	@Override
+//	protected void onStop() {
+//		super.onStop();
+//		
+//	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		
+		if (this.preparationTimer != null) {
+			this.preparationTimer.cancel();
+			this.preparationTimer = null;
+		}
+		if (this.meditationTimer != null) {
+			this.meditationTimer.cancel();
+			this.meditationTimer = null;
+		}
+	}
+}
